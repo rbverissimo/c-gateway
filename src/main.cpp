add2cc513fb1cc9cd6a3be3e920f08bc62bbfcf1
Renato Verissimo <rverissimo.dev@gmail.com>
2025-09-16 14:54:24 -0300

    feat: add cpr and use it to reverse proxy http calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include "crow.h"
+#include <cpr/cpr.h>
 
 int main(){
 
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Available endpoints: \n  1 . /health \n 2. /testing  ";
-    });
+    const char* order_service_env = std::getenv("ORDER_SERVICE_PATH");
+    const std::string ORDER_SERVICE_PATH = order_service_env ? order_service_env : "";
 
     CROW_ROUTE(app, "/health")([](){
         return "API Gateway is healthy";
     });
 
-    CROW_ROUTE(app, "/testing")([](){
-        return "This is just for testing!";
+    CROW_ROUTE(app, "/orders").methods("POST"_method)([&ORDER_SERVICE_PATH](const crow::request& req){
+        cpr::Response r  = cpr::Post(cpr::Url{ORDER_SERVICE_PATH+"/orders"}, 
+            cpr::Body(req.body), 
+            cpr::Header{{"Content-Type", "application/json"}}
+        );
+
+        crow::response res(r.status_code, r.text);
+        res.add_header("Content-Type", "application/json");
+        return res;
     });
+
 
     app.port(18080).multithreaded().run();
 }
